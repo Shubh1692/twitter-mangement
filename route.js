33659609a4e029cb1f7979571e7a473a2088527a
Shubham Vijay Vargiy <svijay1692@gmail.com>
2019-10-04 09:19:36 +0530

    Get user tweets and profile

--- route.js
+++ route.js
@@ -1,7 +1,13 @@
 const userRouter = require('express').Router(),
     jwt = require('jsonwebtoken'),
     Config = require('./app.config'),
-request = require('request');
+    request = require('request'),
+    twitterAPI = require('node-twitter-api'),
+    twitter = new twitterAPI({
+        consumerKey: Config.TWITTER_CONFIG.KEY,
+        consumerSecret: Config.TWITTER_CONFIG.SECRET,
+        callback: Config.TWITTER_CONFIG.CALLBACK,
+    });;
 module.exports = (app, passport, User) => {
     userRouter.route('/request_token')
         .post((req, res) => {
@@ -10,7 +16,7 @@ module.exports = (app, passport, User) => {
                 oauth: {
                     oauth_callback: Config.TWITTER_CONFIG.CALLBACK,
                     consumer_key: Config.TWITTER_CONFIG.KEY,
-                    consumer_secret:  Config.TWITTER_CONFIG.SECRET,
+                    consumer_secret: Config.TWITTER_CONFIG.SECRET,
                 },
                 json: true,
             }, (err, r, body) => {
@@ -27,47 +33,56 @@ module.exports = (app, passport, User) => {
                 url: `https://api.twitter.com/oauth/access_token?oauth_verifier=${req.body.oauth_verifier}`,
                 oauth: {
                     consumer_key: Config.TWITTER_CONFIG.KEY,
-                    consumer_secret:  Config.TWITTER_CONFIG.SECRET,
+                    consumer_secret: Config.TWITTER_CONFIG.SECRET,
                     token: req.body.oauth_token
                 },
                 form: { oauth_verifier: Number(req.body.oauth_verifier) }
             }, async (err, r, body) => {
-
                 try {
                     if (err) {
-                        return res.send(500, { message: err.message });
+                        return res.send(400, { message: err.message });
                     }
                     const bodyString = '{ "' + body.replace(/&/g, '", "').replace(/=/g, '": "') + '"}';
-                    const parsedBody = JSON.parse(bodyString);
-                    req.body['oauth_token'] = parsedBody.oauth_token;
-                    req.body['oauth_token_secret'] = parsedBody.oauth_token_secret;
-                    req.body['user_id'] = parsedBody.user_id;
-                    const user = await User.findOneAndUpdate({
-                        userName: parsedBody.screen_name
-                    }, {
-                        userName: parsedBody.screen_name,
-                        name: "",
-                        twitterProvider: parsedBody
-                    }, {
-                        new: true, upsert: true
-                    });
-                    const token = jwt.sign({
-                        id: user._id
-                    }, Config.EXPRESS_SESSION.SECRET);
-                    return res.status(200).json({
-                        token,
-                        msg: 'Successfully login',
-                        user: user,
-                        success: true
+                    const twitterProvider = JSON.parse(bodyString);
+                    twitter.verifyCredentials(twitterProvider.oauth_token, twitterProvider.oauth_token_secret, {}, async(err, userProfile)=> {
+                        if(err) {
+                            return res.send(400, { message: err.message });
+                        }
+                        const user = await User.findOneAndUpdate({
+                            userName: userProfile.screen_name
+                        }, {
+                            userName: userProfile.screen_name,
+                            name: userProfile.name,
+                            twitterProvider
+                        }, {
+                            new: true, upsert: true
+                        });
+                        const token = jwt.sign({
+                            id: user._id
+                        }, Config.EXPRESS_SESSION.SECRET);
+                        return res.status(200).json({
+                            token,
+                            msg: 'Successfully login',
+                            user: user,
+                            success: true,
+                        });
                     });
                 } catch (error) {
-                    res.send(400, error || err || body || "Error while login with twitter");
+                    res.send(500, error || err || body || "Error while login with twitter");
                 }
             });
         });
     userRouter.route('/getTweets').get(passport.authenticate('jwt', { session: false }), (req, res) => {
-        console.debug(req.user);
-        res.send(200);
+        twitter.getTimeline('user_timeline', {
+            screen_name: req.user.twitterProvider.screen_name
+        }, req.user.twitterProvider.oauth_token, req.user.twitterProvider.oauth_token_secret, (err, tweets) => {
+            if (err) {
+                return res.send(400, 'Error while fetching tweets');
+            }
+            res.send({
+                tweets
+            });
+        })
     });
     app.use('/user/', userRouter);
 }
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import { API_DOMAIN } from '../../constant';
 
 
 class Dashboard extends Component {
     constructor(props) {
         super()
     }
+    async componentDidMount() {
+        await this.getTweets();
+    }
+    /**
+     * @name getTweets
+     * @description
+     * This method used to get tweets
+    */
+
+   async getTweets() {
+    const { cookies } = this.props;
+    const getTweetsRequest = await fetch(`${API_DOMAIN}/user/getTweets`, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${cookies.get('token')}`
+        }
+    });
+    const getTweetsResponse =  await getTweetsRequest.json();
+    console.log('getTweetsResponse', getTweetsResponse);
+}
     render() {
         return (
             <div className="d-flex align-items-center justify-content-center h-100">
