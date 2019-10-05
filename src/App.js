dcd82cc187d01d20a828b18dc49ff256007bc4a2
Shubham Vijay Vargiy <svijay1692@gmail.com>
2019-10-05 22:01:54 +0530

    Add auth befroe load app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import {
   BrowserRouter as Router,
   Route, Redirect, Switch
 } from 'react-router-dom';
+import { Spinner } from 'react-bootstrap';
 import { withCookies } from 'react-cookie';
 import 'bootstrap/dist/css/bootstrap.css';
 import Login from './pages/login';
 import Dashboard from './pages/dashboard';
+import { API_DOMAIN } from './constant';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      authenticateLoading: true
+    }
+  }
+  async componentDidMount() {
+    const { cookies } = this.props;
+    const authenticateRequest = await fetch(`${API_DOMAIN}/user/authenticate`, {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${cookies.get('token')}`
+      }
+    });
+    if (authenticateRequest.status === 401) {
+      cookies.remove('token');
+    }
+    this.setState({
+      authenticateLoading: false
+    })
+  }
+
   render() {
     const { cookies } = this.props;
+    const { authenticateLoading } = this.state;
     const token = cookies.get('token');
+    if (authenticateLoading) {
+      return <Fragment>
+        <Spinner animation="grow" variant="primary" />
+        <Spinner animation="grow" variant="secondary" />
+        <Spinner animation="grow" variant="success" />
+      </Fragment>
+    }
     return (
       <Router>
         {
@@ -23,7 +56,7 @@ class App extends Component {
           <Route exact path='/dashboard' render={() => (<Dashboard {
             ...{
               token,
-              cookies: this.props.cookies
+              cookies
             }
           } />)}></Route>
           <Route render={() => (<Redirect to='/dashboard' />)} />
@@ -31,7 +64,7 @@ class App extends Component {
           <Switch>
             <Route exact path='/login' render={() => (<Login {
               ...{
-                cookies: this.props.cookies
+                cookies
               }
             } />)}></Route>
             <Route render={() => (<Redirect to='/login' />)} />
